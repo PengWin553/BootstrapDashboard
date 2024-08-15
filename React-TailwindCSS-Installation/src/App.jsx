dcc9229d0973502676498dcc5a94581a54f1dd7e
PengWin553 <158315168+PengWin553@users.noreply.github.com>
2024-08-16 01:14:51 +0800

    React-TailwindCSS setup
    using npm for TailwindCSS

---
+++ React-TailwindCSS-Installation/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  
+  return (
+    <div className="bg-black">
+      <h1 className="text-7xl text-center text-blue-400">Hellow Word</h1>
+    </div>
+  )
+}
+
+export default App
